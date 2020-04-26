f5705954d11f7929a0042c99b1d27f51f6091fcc
kunal-motiani <kunal_motiani2002@yahoo.com>
2020-04-26 17:13:05 +0530

    Updates

---
+++ precache-manifest.998dbdadd57cfb9df53ded71bff6f867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3b244a0e4a96eaf41d2ada07c14ef6",
+    "url": "/snakes-and-ladders/index.html"
+  },
+  {
+    "revision": "299e1c13d1b07420aa45",
+    "url": "/snakes-and-ladders/static/css/main.1438feaf.chunk.css"
+  },
+  {
+    "revision": "0de60113de6d451d705f",
+    "url": "/snakes-and-ladders/static/js/2.50cef908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snakes-and-ladders/static/js/2.50cef908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299e1c13d1b07420aa45",
+    "url": "/snakes-and-ladders/static/js/main.80c3bdaf.chunk.js"
+  },
+  {
+    "revision": "1f665f849d05288e8561",
+    "url": "/snakes-and-ladders/static/js/runtime-main.ae48c5a7.js"
+  }
+]);
